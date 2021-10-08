0e31db75f3177c3006960086f5d38948a30a505d
Mackenzie Erickson <77119565+MErickson17@users.noreply.github.com>
2021-10-08 15:28:04 -0600

    added new js files

---
+++ src/js/components/tagline.js
@@ -0,0 +1,6 @@
+const tagline = function(tagline="catchyTagline", className="ui-tagline") {
+    const element = `<p class="${className}">${tagline}</p>`
+    return element
+}
+
+export default tagline
