4f862be27fea0bd5f7abf8ef6df61ebc00c3750f
Mackenzie Erickson <77119565+MErickson17@users.noreply.github.com>
2021-10-14 09:50:17 -0600

    edit to deleteButton component

--- src/js/components/deleteButton.js
+++ src/js/components/deleteButton.js
@@ -4,7 +4,7 @@ import deleteIcon from "./icons/trashIcon";
 const deleteButton = function(className="ui-button") {
     const icon = makeElement(deleteIcon);
     const template = `<button class="${className}">${icon}</button>`;
-    const element = makeElement(element)
+    const element = makeElement(template)
     return element
 }
 
