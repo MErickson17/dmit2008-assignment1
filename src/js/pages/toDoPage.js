2fb7a2f0ddfc2d2fc03ba306dd5828dac9568d47
Mackenzie Erickson <77119565+MErickson17@users.noreply.github.com>
2021-10-14 13:23:25 -0600

    moved data folder

--- src/js/pages/toDoPage.js
+++ src/js/pages/toDoPage.js
@@ -1,18 +1,22 @@
 import branding from "./../components/branding"
 import makeElement from "./../utils/makeElement";
+import dataFetcher from "./../utils/dataFetcher";
 
 
 //the todo list page
-const toDoPage = function() {
+const toDoPage = async function() {
 //doing this for now instead of dataFetcher b/c there seems
 //to be an issue with parcel(?) and using it with local data
-    const todoData = require('./../data/todos.json')
+    //const todoData = require('./../data/todos.json')
+
+    const todoData = await dataFetcher('./data/todos.json');
+    console.log(todoData);
     
     const toDoContent = document.createElement('div') //container for the page
     const brand = branding()
 
     //TODO make a ul component --done
-    //      make addToDo / appBar components
+    //      make addToDo --done / appBar components
     //make a forEach loop on the todoData to:
     // - use the render function to join the template and data
     // - make it into an element
