2dc04ef8317ef7e8aecc92fe2f0190a5d8203d15
Mackenzie Erickson <77119565+MErickson17@users.noreply.github.com>
2021-10-21 21:15:13 -0600

    update to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,23 @@
 import Router from "./routes/router";
 import dataFetcher from "./utils/dataFetcher";
 import { createStore } from "./redux/store";
+import keyGenerator from "./utils/key";
 
 //linked to the web document; inject our app into the html
-const app = document.querySelector('#app')
-Router(window.location.pathname)
+
+
 
 const onAppInit = async function(e){
+    const app = document.querySelector('#app')
     let toDos = await dataFetcher('./data/todos.json')
 
+    //If todo items didn't have a unique id (not necessary for Assignment 1, but good to know):
+    if(toDos[0].id === undefined) {
+        toDos = [...keyGenerator(toDos)]
+    }
+
     createStore(toDos)
+    Router(window.location.pathname)
 }
 
 window.addEventListener('load', onAppInit)
