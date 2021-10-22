1ca734f788fe188146d148271f67464d0efdfc53
Mackenzie Erickson <77119565+MErickson17@users.noreply.github.com>
2021-10-21 21:25:47 -0600

    update to index

--- src/js/index.js
+++ src/js/index.js
@@ -8,13 +8,13 @@ const onAppInit = async function(e){
     const app = document.querySelector('#app')
     let toDos = await dataFetcher('./data/todos.json')
 
-    //If todo items didn't have a unique id (not necessary for Assignment 1, but good to know):hot
+    //If todo items didn't have a unique id (not necessary for Assignment 1, but good to know)
     if(toDos[0].id === undefined) {
         toDos = [...keyGenerator(toDos)]
     }
 
     createStore(toDos)
-    
+
     Router(window.location.pathname)
 }
 
